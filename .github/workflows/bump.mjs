6bce06823dfff004f7be437a6ae9bb554a32247d
Derek Wolpert <me@derekwolpert.com>
2023-12-15 18:05:49 -0500

    add publish workflow

---
+++ .github/workflows/bump.mjs
@@ -0,0 +1,32 @@
+import child from 'node:child_process'
+import { promisify } from 'node:util'
+
+const exec = promisify(function (command, done) {
+  console.log(`Executing: '${command}'`)
+  child.exec(command, (error, stdout, stderr) => {
+    if (error) return done(error)
+    console.error(stderr)
+    console.log(stdout)
+    done(null, { stdout, stderr })
+  })
+})
+
+const versionInput = process.env.VERSION_INPUT
+if (!versionInput) throw new Error('Missing versionInput')
+
+// matches format '1.2.3'
+const latestMatch = /^v([0-9]+)\.([0-9]+)\.([0-9]+)$/
+// matches format '1.2.3-4'
+const nextMatch = /^v([0-9]+)\.([0-9]+)\.([0-9]+)\-([0-9]+)$/
+// matches format '1.2.3-alpha.4'
+const preMatch = /^v([0-9]+)\.([0-9]+)\.([0-9]+)\-[a-zA-Z0-9_]+\.([0-9]+)$/
+
+if (!latestMatch.test(versionInput) && !nextMatch.test(versionInput) && !preMatch.test(versionInput)) {
+  throw new Error('Unable to identify version number format')
+}
+
+const versionNumber = versionInput?.replace(/^v/, '')
+if (!versionNumber) throw new Error('Missing versionNumber')
+
+// bump version, but do not create associated tag/commit
+await exec(`npm --no-git-tag-version version ${versionNumber}`)
