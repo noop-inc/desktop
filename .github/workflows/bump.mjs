18a466cf62522879b3863d659a48125f0cf1771c
Derek Wolpert <me@derekwolpert.com>
2024-01-02 21:59:31 -0500

    add missing v to git push command in bump.mjs

--- .github/workflows/bump.mjs
+++ .github/workflows/bump.mjs
@@ -30,4 +30,4 @@ if (!versionNumber) throw new Error('Missing versionNumber')
 
 // bump version
 await exec(`npm version ${versionNumber}`)
-await exec(`git push origin ${versionNumber} --force`)
+await exec(`git push origin v${versionNumber} --force`)
